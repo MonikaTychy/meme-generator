bf67468113389309956d574d0d8061376557a9db
MonikaTychy <mo.tychy@gmail.com>
2022-11-04 17:32:08 +0100

    prepared basic project structure and styled header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../images/logo.png'
+
+export default function Header() {
+    return (
+        <div className='header'>
+          <img src={logo} alt='troll face' className='header-logo' />
+          <h2 className='header-title'>Meme Generator</h2>
+          <h4>React Course - Project 3</h4>
+        </div>
+    )
+}
