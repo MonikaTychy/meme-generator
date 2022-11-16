a8f9010d1d8af4e9c531d1882db62c8f6ebc21d5
MonikaTychy <mo.tychy@gmail.com>
2022-11-16 18:41:13 +0100

    styling part2 on hard coded data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,10 +4,22 @@ export default function Meme() {
     return (
         <main>
               <form className='form'>
-                <input type='text' className='form-input' placeholder='Top Text' />
-                <input type='text' className='form-input' placeholder='Bottom Text' />
-                <button className='form-button'>Get a new meme image 🖼</button>
+                <input
+                 type='text'
+                 className='form-input' 
+                 placeholder='Top Text' />
+                <input
+                 type='text'
+                 className='form-input' 
+                 placeholder='Bottom Text' />
+                <button
+                 className='form-button'>Get a new meme image 🖼</button>
               </form>
+              <div className='meme'>
+                 <img src='http://i.imgflip.com/1bij.jpg' alt="funny situation with some joke" />
+                 <h2 className='meme-text top'>top text</h2>
+                 <h2 className='meme-text bottom'>bottom text</h2>
+              </div>
         </main>
     )
 }
